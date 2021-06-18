c9ca40f3f2ca64974f463d616fcc58f6c3ea53cd
HarmKraats <78961607+HarmKraats@users.noreply.github.com>
2021-06-18 14:03:12 +0200

    fortmat
    inlog bestanden geformateerd

--- PHP/inlogen/db.inc.php
+++ PHP/inlogen/db.inc.php
@@ -1,17 +1,13 @@
 <?php
-  //db functie
-  function getDbConnection() {
-  $host = 'localhost';
-  $user = 'root';
-  $pwd = '';
-  $db = 'game';
-  
-  $con = new PDO('mysql:host='.$host.';charset=utf8;dbname='.$db, $user, $pwd);
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //db functie
+    function getDbConnection() {
+    $host = 'localhost';
+    $user = 'root';
+    $pwd = '';
+    $db = 'game';
+    
+    $con = new PDO('mysql:host='.$host.';charset=utf8;dbname='.$db, $user, $pwd);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  return $con;
-  }
-
-
-
-  ?>
+    return $con;
+    }
--- PHP/inlogen/login.php
+++ PHP/inlogen/login.php
@@ -7,53 +7,54 @@ require_once 'inc/gamedata.inc.php';
 //db functie aanroepen
 $dbh = getDbConnection();
 
-if(isset($_POST['submit']) && ! empty($_POST['username'])) {
+if (isset($_POST['submit']) && !empty($_POST['username'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $sql = "SELECT username, password
             FROM player
-            WHERE username = '".$username."'";
-    
+            WHERE username = '" . $username . "'";
+
     $result = $dbh->query($sql);
 
-    if($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        if($row['password'] == $password) {
+    if ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        if ($row['password'] == $password) {
             session_start();
             $_SESSION['username'] = $username;
             header('Location: char_type.php');
-        }
-        
-        else {
+        } else {
             echo "Ongeldige login!";
         }
-    }
-    else {
+    } else {
         echo "Ongeldige login";
     }
 }
 ?>
 <!DOCTYPE html>
 <html>
-<head> 
-<title> Inloggen </title> 
+
+<head>
+    <title> Inloggen </title>
 </head>
+
 <body>
-<link rel='stylesheet' type='text/css' href='style.css'>
-<h1>Welkom en Log hier in voor DE GAME</h1>
-<form name="form1" method="post" action="login.php">
-<label for="username">Username:<label><br>
-<input type="text" id="username" name="username" value=""><br>
-<label for="password">Password:<label><br>
-<input type="password" id="password" name="password" value=""><br>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+    <h1>Welkom en Log hier in voor DE GAME</h1>
+    <form name="form1" method="post" action="login.php">
+        <label for="username">Username:<label><br>
+                <input type="text" id="username" name="username" value=""><br>
+                <label for="password">Password:<label><br>
+                        <input type="password" id="password" name="password" value=""><br>
 
-<?php
+                        <?php
+                        
 
+                        ?>
 
-?>
+                        <input type="submit" name="submit" value="Verzenden">
 
-<input type="submit" name="submit" value="Verzenden">
-
-</form>
+    </form>
 </body>
-</body></html>
+</body>
+
+</html>
